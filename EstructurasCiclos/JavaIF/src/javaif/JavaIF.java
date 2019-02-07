d67a820d9e1a12d6fef080855f60c56dcff825fb
jaespinol <jaespinolgit@gmail.com>
2019-02-07 11:08:38 -0600

    Actualizacion
    Mensaje Hola Mundo

--- EstructurasCiclos/JavaIF/src/javaif/JavaIF.java
+++ EstructurasCiclos/JavaIF/src/javaif/JavaIF.java
@@ -5,6 +5,8 @@
  */
 package javaif;
 
+import java.util.Scanner;
+
 /**
  *
  * @author IngEspino
@@ -16,6 +18,9 @@ public class JavaIF {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner comienzo = new Scanner (System.in);
+        System.out.println();
+        // Hola Mundo
     }
     
 }
