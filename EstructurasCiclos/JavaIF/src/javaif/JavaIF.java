e219eaa941dcf7ef5779bc87088d88be6545f9ce
jaespinol <jaespinolgit@gmail.com>
2019-02-11 09:19:08 -0600

    Actualizacion de Ejemplos
    Actualizacion de Ejemplos

--- EstructurasCiclos/JavaIF/src/javaif/JavaIF.java
+++ EstructurasCiclos/JavaIF/src/javaif/JavaIF.java
@@ -16,17 +16,37 @@ public class JavaIF {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-         Scanner comienzo = new Scanner (System.in);
+        MayorEdad();
+        // Hola Mundo
         Scanner Nom = new Scanner (System.in);
-         System.out.print("Ingrese su edad: ");
-         int edad = comienzo.nextInt();
+        System.out.println("Cual es tu Nombre ");
+        String Nombre = Nom.nextLine();
+        System.out.println("Hola " + Nombre + " como estas !");
+        
+        NumeroMayotyMenor();
+    }
+    
+    public static void MayorEdad() {
+        Scanner comienzo = new Scanner (System.in);
+        System.out.print("Ingrese su edad: ");
+        int edad = comienzo.nextInt();
         if (edad >= 18) 
             System.out.println("Ud. es mayor de Edad !");
         else 
             System.out.println("Ud. es menor de Edad !");
-        // Hola Mundo
-        System.out.println("Cual es tu Nombre ");
-         String Nombre = Nom.nextLine();
-         System.out.println("Hola " + Nombre);
+    }
+
+    private static void NumeroMayotyMenor() {
+        Scanner comienzo = new Scanner (System.in);
+        System.out.println("\nEste Metodo sirve para hallar cual es el numero mayor");
+        int num1, num2;
+        System.out.println("Por Favor de digitar el primer Numero");
+        num1 = comienzo.nextInt();
+        System.out.println("Por Favor de digitar el segundo Numero");
+        num2 = comienzo.nextInt();
+        if (num1 > num2) 
+            System.out.println("El Numero Mayor es: " + num1 + " !");
+        else 
+            System.out.println("El Numero Mayor es: " + num2 + " !");
     }
 }
---
+++ EstructurasCiclos/JavaIFAnidado/src/javaifanidado/JavaIFAnidado.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaifanidado;
+
+import java.util.*;
+
+/**
+ *
+ * @author IngEspino
+ */
+public class JavaIFAnidado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner comienzo = new Scanner (System.in);
+        System.out.println("Este programa sirve para hallar el indice de la masa corporal de una persona");
+        double imc, peso, estatura;
+        
+        System.out.println("Por favor digite el peso de una Persona");
+        peso = comienzo.nextDouble();
+        
+        System.out.println("Por favor digite la Estatura");
+        estatura = comienzo.nextDouble();
+        
+        imc = peso / estatura;
+        System.out.println("El IMC de una persona es: " + imc);
+        
+        if (imc < 16) {
+            System.out.println("El peso del paciente es Muy Bajo");
+        }else 
+            if (imc == 16 || imc == 18.5) {
+                System.out.println("El peso del paciente Bajo");
+        }else
+                if (imc == 18.6 || imc == 24.9) {
+                    System.out.println("El peso del paciente es Normal");
+        }else
+                if (imc == 25 || imc == 29.9) {
+                    System.out.println("El peso del paciente sufre de Sobre Peso");
+        }else
+                    if (imc > 30) {
+                        System.out.println("El peso del paciente sufre de Obesidad");
+        }
+    }
+    
+}
